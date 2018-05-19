458cc51df173899ecdc0d5f7a9abb1e283f83f1d
bedlansonny <bedlansonny@gmail.com>
2018-05-18 20:49:07 -0500

    notes

---
+++ src/Tester.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+
+import java.net.URL;
+
+public class Tester
+{
+    public static void main(String args[]) throws IOException
+    {
+        //Scanner in = new Scanner(System.in);
+        Picture pic = new Picture("party.jpg");
+
+
+    }
+}
